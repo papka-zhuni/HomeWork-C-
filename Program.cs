b8d992c2c076ff47efd7eb611cdb5418775d1b20
papka-zhuni <74926516+papka-zhuni@users.noreply.github.com>
2020-11-23 20:58:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите своё имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Приветсвуем, {name} ");
+        }
+    }
+}
